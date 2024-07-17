5695fb0c6446a1e409e311a252239b98541be6fc
AAKARSH <akk2080@gmail.com>
2024-07-17 22:08:29 +0530

    first

---
+++ src/main/java/com/webapp/springboot/WhatToDoApplication/WhatToDoApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.springboot.WhatToDoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToDoApplication.class, args);
+	}
+
+}
