a4fb9025246b42ace10386b8688323472752cfee
AAKARSH <akk2080@gmail.com>
2024-07-25 18:57:04 +0530

    development in progress, Spring Security added

---
+++ src/main/java/com/webapp/springboot/WhatToDoApplication/login/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.webapp.springboot.WhatToDoApplication.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String goToWelcomePage(ModelMap model){
+        model.put("name", "Akk");
+
+        return "welcome";
+    }
+
+
+}
